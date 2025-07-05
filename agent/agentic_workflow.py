41d8c3ed8a86798b510e946db9591ee97f483762
your-huggingface-username <your-email@example.com>
2025-07-05 20:30:24 +0530

    initiated agentic workflow and tool developement

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -37,6 +37,8 @@ class GraphBuilder():
         input_question = [self.system_prompt] + user_question
         response = self.llm_with_tools.invoke(input_question)
         return {"messages": [response]}
+    
+    
     def build_graph(self):
         graph_builder=StateGraph(MessagesState)
         graph_builder.add_node("agent", self.agent_function)
@@ -45,8 +47,11 @@ class GraphBuilder():
         graph_builder.add_conditional_edges("agent",tools_condition)
         graph_builder.add_edge("tools","agent")
         graph_builder.add_edge("agent",END)
+        
+        
         self.graph = graph_builder.compile()
         return self.graph
+        
         
     def __call__(self):
         return self.build_graph()
